7beca58b16dd4b7d2a6a5bbbba3946481512389a
Bryne <babasalancebryne@gmail.com>
2024-05-21 18:24:46 +0800

    Polished NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,128 @@
 <template>
+  <div class="container-fluid" style="background: #161827">
+    <div class="container">
+      <div class="row">
+        <div
+          class="col-xl-3 col-lg-6 col-sm-12 d-flex justify-content-center align-items-center"
+        >
+          <div
+            class="email p-2 d-flex justify-content-center align-items-center"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="#57b957"
+              class="bi bi-envelope-fill"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z"
+              />
+            </svg>
+            <a href="#" class="font-3 white-color" style="margin-left: 5px">
+              Support@example.com
+            </a>
+          </div>
+        </div>
+
+        <div
+          class="col-xl-4 col-lg-6 col-sm-12 d-flex justify-content-center align-items-center"
+        >
+          <div
+            class="location p-2 d-flex justify-content-center align-items-center"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="#57b957"
+              class="bi bi-globe-asia-australia"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="m10.495 6.92 1.278-.619a.483.483 0 0 0 .126-.782c-.252-.244-.682-.139-.932.107-.23.226-.513.373-.816.53l-.102.054c-.338.178-.264.626.1.736a.48.48 0 0 0 .346-.027ZM7.741 9.808V9.78a.413.413 0 1 1 .783.183l-.22.443a.6.6 0 0 1-.12.167l-.193.185a.36.36 0 1 1-.5-.516l.112-.108a.45.45 0 0 0 .138-.326M5.672 12.5l.482.233A.386.386 0 1 0 6.32 12h-.416a.7.7 0 0 1-.419-.139l-.277-.206a.302.302 0 1 0-.298.52z"
+              />
+              <path
+                d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0M1.612 10.867l.756-1.288a1 1 0 0 1 1.545-.225l1.074 1.005a.986.986 0 0 0 1.36-.011l.038-.037a.88.88 0 0 0 .26-.755c-.075-.548.37-1.033.92-1.099.728-.086 1.587-.324 1.728-.957.086-.386-.114-.83-.361-1.2-.207-.312 0-.8.374-.8.123 0 .24-.055.318-.15l.393-.474c.196-.237.491-.368.797-.403.554-.064 1.407-.277 1.583-.973.098-.391-.192-.634-.484-.88-.254-.212-.51-.426-.515-.741a7 7 0 0 1 3.425 7.692 1 1 0 0 0-.087-.063l-.316-.204a1 1 0 0 0-.977-.06l-.169.082a1 1 0 0 1-.741.051l-1.021-.329A1 1 0 0 0 11.205 9h-.165a1 1 0 0 0-.945.674l-.172.499a1 1 0 0 1-.404.514l-.802.518a1 1 0 0 0-.458.84v.455a1 1 0 0 0 1 1h.257a1 1 0 0 1 .542.16l.762.49a1 1 0 0 0 .283.126 7 7 0 0 1-9.49-3.409Z"
+              />
+            </svg>
+            <a href="#" class="font-3 white-color" style="margin-left: 5px">
+              Kleine Pierboulevard 8-6 2249 KV Vries
+            </a>
+          </div>
+        </div>
+
+        <div class="col-xl-5 col-lg-12 icon-three">
+          <div class="icons">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              class="bi bi-meta"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M8.217 5.243C9.145 3.988 10.171 3 11.483 3 13.96 3 16 6.153 16.001 9.907c0 2.29-.986 3.725-2.757 3.725-1.543 0-2.395-.866-3.924-3.424l-.667-1.123-.118-.197a55 55 0 0 0-.53-.877l-1.178 2.08c-1.673 2.925-2.615 3.541-3.923 3.541C1.086 13.632 0 12.217 0 9.973 0 6.388 1.995 3 4.598 3q.477-.001.924.122c.31.086.611.22.913.407.577.359 1.154.915 1.782 1.714m1.516 2.224q-.378-.614-.727-1.133L9 6.326c.845-1.305 1.543-1.954 2.372-1.954 1.723 0 3.102 2.537 3.102 5.653 0 1.188-.39 1.877-1.195 1.877-.773 0-1.142-.51-2.61-2.87zM4.846 4.756c.725.1 1.385.634 2.34 2.001A212 212 0 0 0 5.551 9.3c-1.357 2.126-1.826 2.603-2.581 2.603-.777 0-1.24-.682-1.24-1.9 0-2.602 1.298-5.264 2.846-5.264q.136 0 .27.018Z"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              class="bi bi-twitter-x"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              class="bi bi-vimeo"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M15.992 4.204q-.106 2.334-3.262 6.393-3.263 4.243-5.522 4.243-1.4 0-2.367-2.583L3.55 7.523Q2.83 4.939 2.007 4.94q-.178.001-1.254.754L0 4.724a210 210 0 0 0 2.334-2.081q1.581-1.364 2.373-1.437 1.865-.185 2.298 2.553.466 2.952.646 3.666.54 2.447 1.186 2.445.5 0 1.508-1.587 1.006-1.587 1.077-2.415.144-1.37-1.077-1.37a3 3 0 0 0-1.185.261q1.183-3.86 4.508-3.756 2.466.075 2.324 3.2z"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              class="bi bi-pinterest"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M8 0a8 8 0 0 0-2.915 15.452c-.07-.633-.134-1.606.027-2.297.146-.625.938-3.977.938-3.977s-.239-.479-.239-1.187c0-1.113.645-1.943 1.448-1.943.682 0 1.012.512 1.012 1.127 0 .686-.437 1.712-.663 2.663-.188.796.4 1.446 1.185 1.446 1.422 0 2.515-1.5 2.515-3.664 0-1.915-1.377-3.254-3.342-3.254-2.276 0-3.612 1.707-3.612 3.471 0 .688.265 1.425.595 1.826a.24.24 0 0 1 .056.23c-.061.252-.196.796-.222.907-.035.146-.116.177-.268.107-1-.465-1.624-1.926-1.624-3.1 0-2.523 1.834-4.84 5.286-4.84 2.775 0 4.932 1.977 4.932 4.62 0 2.757-1.739 4.976-4.151 4.976-.811 0-1.573-.421-1.834-.919l-.498 1.902c-.181.695-.669 1.566-.995 2.097A8 8 0 1 0 8 0"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              class="bi bi-google"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M15.545 6.558a9.4 9.4 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.7 7.7 0 0 1 5.352 2.082l-2.284 2.284A4.35 4.35 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.8 4.8 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.7 3.7 0 0 0 1.599-2.431H8v-3.08z"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
   <div class="container">
     <div class="row">
       <nav
         :class="{ scrolled: isScrolled }"
-        class="navbar navbar-expand-lg navbar-light w-100"
+        class="navbar navbar-expand-lg navbar-light w-100 d-flex"
       >
-        <div class="col-lg-3 col-sm-12 d-flex justify-content-center">
+        <div class="col-lg-3 col-sm-12 d-flex justify-content-between">
           <router-link class="navbar-brand" to="/">
             <img src="../assets/logo.png" alt="logo" />
           </router-link>
@@ -49,7 +166,7 @@
             </div>
           </div>
         </div>
-        <div class="col-xl-3 col-12">
+        <div class="col-xl-3">
           <div
             class="button-cart d-flex justify-content-center align-items-center"
           >
@@ -68,7 +185,21 @@
               </svg>
             </div>
             <div class="divider"></div>
-            <button class="btn-green">(0121) 268 3610</button>
+            <button class="btn-green text-wrap" style="font-weight: 500">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="currentColor"
+                class="bi bi-headphones"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  d="M8 3a5 5 0 0 0-5 5v1h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V8a6 6 0 1 1 12 0v5a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h1V8a5 5 0 0 0-5-5"
+                />
+              </svg>
+              (0121) 268 3610
+            </button>
           </div>
           <div class="button-nav">
             <div class="collapse-layout">
@@ -128,6 +259,36 @@ const isActive = (path) => {
 </script>
 
 <style scoped>
+.icon-three {
+  display: flex;
+  justify-content: end;
+  align-items: center;
+}
+.header1 {
+  display: flex;
+  flex-direction: column;
+}
+.icons svg {
+  fill: #b2b3ba;
+  cursor: pointer;
+  margin: 10px;
+  transition: 0.4s ease-in-out;
+}
+
+.icons svg:hover {
+  fill: #57b957;
+}
+.email {
+  margin-right: 40px;
+}
+.container-fluid a {
+  text-decoration: none;
+  color: rgb(154, 150, 150);
+}
+
+.container-fluid a:hover {
+  color: #57b957;
+}
 .act .nav-link {
   color: #57b957;
 }
@@ -156,6 +317,9 @@ const isActive = (path) => {
 @media (max-width: 1200px) {
   .nav-link {
     width: 100px;
+  }
+  .icon-three {
+    justify-content: center;
   }
 }
 
@@ -226,12 +390,7 @@ a {
 .button-nav {
   display: none;
 }
-@media (max-width: 992px) {
-  .collapse-layout {
-    display: hidden;
-    justify-content: start;
-  }
-}
+
 .navbar {
   opacity: 0.9;
 }
@@ -247,6 +406,7 @@ a {
   opacity: 1;
   padding: 0;
   box-shadow: rgba(65, 65, 65, 0.472);
+  transform: translateY(-41px);
 }
 
 .navbar.scrolled .button-cart {
@@ -319,10 +479,16 @@ a {
     left: 0;
   }
 }
+@media (max-width: 1200px) {
+  .navbar.scrolled {
+    display: none !important;
+  }
+}
 
 @media (max-width: 992px) {
-  .navbar.scrolled {
-    display: none;
+  .collapse-layout {
+    display: hidden;
+    justify-content: start;
   }
 }
 
